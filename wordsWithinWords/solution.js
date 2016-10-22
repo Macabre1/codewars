3c40ea643635f82c953ed18b4aa96e41b45ff9cf
Aaron Ventura <aaronjosephventura@gmail.com>
2016-10-22 15:27:21 -0500

    solution for words within words complete

---
+++ wordsWithinWords/solution.js
@@ -0,0 +1,15 @@
+function nestedWordCount (wordList) {
+  var biggest = [-1, ''];
+  wordList.forEach(function(word) {
+    var count = 0;
+    wordList.forEach(function(word2) {
+      if(word.indexOf(word2) > -1) {
+        count++;
+      }
+    });
+    if(count > biggest[0]) {
+      biggest = [count, word];
+    }
+  });
+  return biggest[1];
+};
