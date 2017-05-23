5ec47cac0bed7ba2b9959669b871a2fa8b93999c
Aaron Ventura <aaronjosephventura@gmail.com>
2017-05-23 15:16:02 -0500

    Update simpleSolution.js

--- fizzBuzz/simpleSolution.js
+++ fizzBuzz/simpleSolution.js
@@ -1,10 +1,10 @@
-function FizzBuzz (value) {
-  if (value % 3 === 0 && value % 5 !== 0) {
-    return("Fizz");
-  } else if (value % 5 === 0 && value % 3 !== 0) {
-    return("Buzz");
-  } else if (value % 5 === 0 && !value % 3 === 0) {
+function fizzBuzz (value) {
+  if (value % 5 === 0 && value % 3 === 0) {
     return("FizzBuzz");
+  } else if (value % 5 === 0) {
+    return("Buzz");
+  } else if (value % 3 === 0) {
+    return("Fizz");
   } else {
    return(value);
   }
